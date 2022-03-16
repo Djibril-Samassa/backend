407e86251c526fcdb6051f0be7b8745fb214966d
Djibril <djibrilsamassa.pro@gmail.com>
2022-03-16 14:57:39 +0100

    uploads commit

---
+++ chapitre_04/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const upload = multer({ dest: 'public/uploads/' });
+
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.post("/image", upload.single("image"), (req, res) => {
+	fs.renameSync(
+		req.file.path,
+		path.join(req.file.destination, req.file.originalname)
+	);
+
+	res.send("Image received");
+});
+
+app.listen(8000, ()=>
+    console.log("Listening")
+)
