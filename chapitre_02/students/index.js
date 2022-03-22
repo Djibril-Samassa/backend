34892056fbe2f48fc95f8164e222de1f090d50af
Djibril <djibrilsamassa.pro@gmail.com>
2022-03-22 14:36:20 +0100

    students database commit

--- chapitre_02/students/index.js
+++ chapitre_02/students/index.js
@@ -8,40 +8,57 @@ const { Pool } = require("pg")
 app.use(express.json());
 const Postgres = new Pool({ ssl: { rejectUnauthorized: false } });
 
-app.use((req, res, next) => {
-	console.log("requête reçue");
-	next();
-});
 
-const students = [
-	{
-		id: 1,
-		name: "Nicolas",
-	},
-	{
-		id: 2,
-		name: "Anita",
-	},
-	{
-		id: 3,
-		name: "Djibril",
-	},
-];
+app.get("/", async(req, res) =>{
+    let students;
+	try{	
+		students = await Postgres.query("SELECT * FROM students")
+	} catch(err){
+		console.log(err);
+		return res.status(400).json({
+			message: "An error Happened"
+		})
+	}
 
-app.get("/", (req, res) =>{
-    res.send(students);
+	res.json(students.rows)
 })
 
-app.get("/students", (req, res) =>{
-    res.send(students);
+app.get("/students/:id", async(req, res) =>{
+    let student;
+	try{
+		student= await Postgres.query("SELECT * FROM students WHERE id=$1",[req.params.id]);
+	} catch(err){
+		console.log(err);
+		return res.status(400).json({
+			message: "An error happened"
+		})
+	}
+
+	res.send(student.rows)
 })
 
-app.post("/students", (req,res) => {
-    students.push({
-        id: students.length + 1,
-        name: req.body.name,
-    });
-    res.send(students)
+app.post("/students", async(req,res) => {
+    let postStudent;
+	try{postStudent= await Postgres.query("INSERT INTO students(name, city, age) VALUES($1, $2, $3)",
+	[req.body.name, req.body.city, req.body.age]);
+	}catch(err){
+		console.log(err);
+		return res.status(400).json({
+			message: "An error happened"
+		})
+	}
+
+	let displayStudents;
+	try{	
+		displayStudents = await Postgres.query("SELECT * FROM students")
+	} catch(err){
+		console.log(err);
+		return res.status(400).json({
+			message: "An error Happened"
+		})
+	}
+
+	res.send(displayStudents.rows)
 })
 
 
